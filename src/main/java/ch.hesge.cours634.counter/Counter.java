6978a96330d35b514b13c8b13e999e883467939a
francoisnobileheg <francois.nobile@etu.hesge.ch>
2020-02-20 16:36:02 +0100

    TP Version 1

--- src/main/java/ch.hesge.cours634.counter/Counter.java
+++ src/main/java/ch.hesge.cours634.counter/Counter.java
@@ -2,4 +2,24 @@ package ch.hesge.cours634.counter;
 
 public class Counter implements ICounter {
 
+    private int cpt;
+
+    public Counter(int depart) {
+        this.cpt = depart;
+    }
+
+    @Override
+    public void inc() throws CounterException {
+        this.cpt += 1;
+    }
+
+    @Override
+    public void add(int step) throws CounterException {
+        this.cpt += step;
+    }
+
+    @Override
+    public int getValue() {
+        return cpt;
+    }
 }
--- src/main/java/ch.hesge.cours634.counter/UpperLimitedPositiveCounter.java
+++ src/main/java/ch.hesge.cours634.counter/UpperLimitedPositiveCounter.java
@@ -1,6 +1,37 @@
 package ch.hesge.cours634.counter;
 
-public class UpperLimitedPositiveCounter extends ICounter {
+public class UpperLimitedPositiveCounter extends Counter {
 
+    private int cpt;
+    private int max;
+    private final int MIN = 0;
 
+    public UpperLimitedPositiveCounter(int depart, int max) {
+        super(depart);
+        this.max = max;
+    }
+
+    @Override
+    public void inc() throws CounterException {
+        if (this.cpt + 1 > this.max) {
+            throw new CounterException("Error : The value of the counter can't be increased more.");
+        }
+        this.cpt += 1;
+    }
+
+    @Override
+    public void add(int step) throws CounterException {
+        if (this.cpt + step > this.max) {
+            throw new CounterException("Error : The value of the counter can't be increased more.");
+        }
+        if (this.cpt + step < MIN) {
+            throw new CounterException("Error : The value of the counter can't be under " + MIN + ".");
+        }
+        this.cpt += step;
+    }
+
+    @Override
+    public int getValue() {
+        return this.cpt;
+    }
 }
